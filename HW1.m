5856a052dba9c826d0d9fd2830499f2af4b8baf7
Björn Viberg <bjorn.viberg@outlook.com>
2018-09-11 22:41:11 +0200

    Changed hebbsRule to accomodate more than one pattern. Initial attempts to create a script for error estimation

--- HW1.m
+++ HW1.m
@@ -1,6 +1,18 @@
 %% generate patterns
 p=[12,20,40,60,80,100];
+N=100;
+patterns=genPatterns(N,p);
+%loopa över p
+err=int8(zeros(1,length(p)));
+for ii=p
+   for jj=1:1e5
+       c=
+       err(ii) = err(ii)+int8(c>1);
+   end
+end
+%för varje p: genererra pattern och beräkna asynkron dynamik 1e5 ggr. approx P_error genom
+%andelen fel (enligt ekv 2.33). 
 
-aa=[1,2];
-index =[1 1 2 2 1;2 2 2 2 2];
-bb=aa(index)
+%% Asynchron dynamic (se eq 2.8)
+
+%% Ekvation 2.33 beskriver om n'tverket har gjort ett fel (if Ci>1 then fel)
--- hebbsRule.m
+++ hebbsRule.m
@@ -1,10 +1,14 @@
 function [weights] = hebbsRule(pattern,diagZero)
-%UNTITLED4 This function returns the weights that store the pattern
+%HEBBSRULE This function returns the weights that store the pattern
 %according to hebbs rule. If diagZero is true then w_ii = 0.
-pattern=pattern(:); % ensure column vector
-weights = pattern*pattern'/(length(pattern)^2);
-if diagZero
-   weights= weights-eye(length(pattern))/(length(pattern)^2);
+
+[N,p]=size(pattern);
+for i=1:p
+    weights = weights + pattern(:,i)*pattern(:,i)';
 end
+if diagZero
+   weights(logical(eye(N)))= 0;
+end
+weights=weights/N;
 end
 
