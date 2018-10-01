e2f37c44c861b4525ed743a084eca675639d2c65
Björn Viberg <bjorn.viberg@outlook.com>
2018-10-01 22:29:45 +0200

    Attempt to solve hw2b. Open issue: H increases rather than the oposite

--- HA2/HW2b.m
+++ HA2/HW2b.m
@@ -1,28 +1,49 @@
+% Create targets and outputs
 targets=[[1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1];...
-        [-1, -1, 1, 1, 1, -1, -1, -1, -1, 1, 1, -1, 1, 1, 1, -1],...
-        [-1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, 1, 1],...
-        [1, 1, 1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, -1, 1],...
-        [-1, -1, 1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1],...
-        [-1, 1, -1, -1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1]];
-
+    [-1, -1, 1, 1, 1, -1, -1, -1, -1, 1, 1, -1, 1, 1, 1, -1];...
+    [-1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, 1, 1];...
+    [1, 1, 1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, -1, 1];...
+    [-1, -1, 1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1];...
+    [-1, 1, -1, -1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1]];
 M=csvread('input_data_numeric.csv',0,1)';
-a=-0.2;
-b=0.2;
+
+% Define network påarameters and initialise.
 beta=0.5;
 learnRate=0.02;
+a=-0.2;
+b=0.2;
 Network.W= a + (b-a).*rand(1,4); %uniform [-0.2,0.2]
 Network.bias=5*(a + (b-a).*rand(1));%uniform [-1,1]
-Network.output=tanh(beta*(Network.W*M(:,1)+Network.theta));
-%dWi=learnRate*sum(target-output)*(1-tanh())
-dW=zeros(4,1);
-dBias=0;
-targetIndex=1;
-for ii=1:1e5
-    randMu=randi(16);
-    randOutput=tanh(beta*(Network.W*M(:,randMu)+Network.theta));
-    randTraget=targets()
-    dW = learnRate*beta*(randTarget-randOutput)
-    dBias = 
-    Network.W=Network.W+dW;
-    Network.bias=Network.bias+dBias;
+errorVec=zeros(length(targets(:,1)),1);% Vector to store number of wrongly classified inputs
+
+for t_ix=1:length(targets(:,1))
+    t=targets(t_ix,:);
+    for ii=1:1e5
+        Network.output=tanh(beta*(Network.W*M+Network.bias)); % Compute output of all inputs
+        
+        H=0.5*sum((Network.output-t).^2)
+        errorVec(t_ix)=0.5*sum(abs(signum(Network.output)-t)); % Store current error value
+        if errorVec(t_ix)==0
+            disp('Sluta!!!')
+            break;
+        end
+            
+        %Store network parameters before update
+        weights=Network.W;
+        bias=Network.bias;
+        
+        % Update weights based on randomly choosen input
+        randMu=randi(16);
+        A=t(randMu)- Network.output(randMu);
+        B=1-tanh(Network.W*M(:,randMu))^2;
+        differentialUpdate = learnRate*beta*A*B*[M(:,randMu);-1]; % The bias can be viewed as a constant negative 
+        Network.W=Network.W + differentialUpdate(1:end-1)';
+        Network.bias=Network.bias+differentialUpdate(end);
+    end
+end
+
+function [signX] = signum(x)
+% SIGNUM returns sign of input(>0 => 1,<0 => -1). signum(0) will return 1
+signX=sign(x);
+signX(signX==0)=-1;
 end
